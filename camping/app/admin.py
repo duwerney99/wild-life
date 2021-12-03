403c2e02e4d28467d2cc959ecba631b699db9ec5
Santiago <d.hernandez1068@pascualbravo.edu.co>
2021-12-02 21:06:22 -0500

    first commit

--- camping/app/admin.py
+++ camping/app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+
+class UsuariosResource(resources.ModelResource):
+    class Meta:
+        model = Usuarios
+
+
+class UsuariosAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['nombre_completo']
+    list_display = ('nombre_completo', 'state', 'fecha_usuario')
+    resources_class = UsuariosResource
+
+
+class PublicacionesResource(resources.ModelResource):
+    class Meta:
+        model = Publicaciones
+
+
+
+class PublicacionesAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['nombre_lugar']
+    list_display = ('id','nombre_lugar', 'nombre_guia', 'fecha_publicacion')
+    resources_class = PublicacionesResource
+
+admin.site.register(Usuarios, UsuariosAdmin)
+admin.site.register(Publicaciones, PublicacionesAdmin)
--- camping/app/models.py
+++ camping/app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Usuarios(models.Model):
+    ''''
+        Este modelo esta enfocado en la creacion de usuario el cual contiene campos adicciones a los genericos.
+        Fecha de creacion: 1/12/2021
+        autores: Stiven Restrepo, Santiago Hernandez
+    '''
+    sexos = (
+        ('Femenino', 'femenino'), 
+        ('Masculino', 'masculino'),
+        ('Otro', 'otro'), 
+        ('No definido', 'no_definido'),  
+    )
+    sexo = models.CharField(max_length=20, blank=False, null=False, choices=sexos)
+    id = models.AutoField(primary_key= True)
+    nombre_completo = models.CharField('Nombre completo', max_length=30, blank=False, null=False)
+    state = models.BooleanField('Usuario Activado/Usuario no Activado', default=True)
+    fecha_usuario = models.DateField('Fecha de Creacion', auto_now=False, auto_now_add=True)
+    telefono = models.IntegerField('Telefono', blank=False, null=False)
+
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    def __str__(self):
+        return self.nombre_completo
+
+
+class Publicaciones(models.Model):
+    id = models.AutoField(primary_key= True)
+    nombre_lugar = models.CharField(max_length=120, blank=False, null=False)
+    ubicacion = models.CharField(max_length=250, blank=False, null=False)
+    fotos = models.ImageField(upload_to='documents/wild-life-v1/app/static/images/')
+    videos = models.FileField(upload_to='documents/wild-life-v1/app/static/videos/')
+    descripcion = models.CharField(max_length=250, blank=False, null=False)
+    fecha_publicacion = models.DateField('Fecha de publicacion', auto_now=False, auto_now_add=True)
+    nombre_guia = models.CharField('Guia',max_length=120, blank=False, null=False)
+    nombre_usuario = models.ForeignKey(Usuarios, on_delete = models.CASCADE)
+
+
+    class Meta:
+        verbose_name = 'Publicacion'
+        verbose_name_plural = 'Publicaciones'
+
+    def __str__(self):
+        return self.nombre_lugar
